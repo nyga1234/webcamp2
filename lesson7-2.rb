94633c80ba0aadc3a6cee52440f9e94f5fd7e8a6
nyga1234 <nyga1234@icloud.com>
2021-07-23 00:25:43 +0000

    7/23

--- lesson7-2.rb
+++ lesson7-2.rb
@@ -1,25 +1,27 @@
 puts "計算をはじめます"
+
 puts "何回計算を繰り返しますか?"
 
-input = gets.to_i
+kurikaeshi = gets.to_i
 
 i = 1
-while i <= input do
+while i <= kurikaeshi do
   puts "#{i}回目の計算"
-  puts "２つの値を入力してください"
+
+puts "2つの値を入力してください"
 
 a = gets.to_i
 b = gets.to_i
 
-puts "a=#{a}"
+puts "a-#{a}"
 puts "b=#{b}"
 
 puts "計算結果を出力します"
 
-puts "a+b=#{a + b}"
-puts "a-b=#{a - b}"
-puts "a*b=#{a * b}"
-puts "a/b=#{a / b}"
+puts "a+b=#{a+b}"
+puts "a-b=#{a-b}"
+puts "a*b=#{a*b}"
+puts "a/b=#{a/b}"
 
 i += 1
 end
