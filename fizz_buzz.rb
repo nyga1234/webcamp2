bad37f45d48265a5565002b47507ff8479a4f4a2
nyga1234 <nyga1234@icloud.com>
2021-07-27 16:21:21 +0000

    7/28

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,18 +1,23 @@
 def fizz_buzz(number)
+
   if number % 15 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
-      "Fizz"
+    puts "FizzBuzz"
+
+  elsif number %  3 == 0
+    puts "Fizz"
+
   elsif number % 5 == 0
-        "Buzz"
+    puts "Buzz"
+
   else
-        number.to_s
+    puts "#{number}"
   end
+
 end
 
-puts "数字を入力してください。"
+puts "数字を入力してください"
 
 input = gets.to_i
 
-puts "結果は..."
+puts "結果は"
 puts fizz_buzz(input)
